2c73973683d2d24cfe1f60e9ab185987512d293b
dpqlsn <a01037111209@gmail.com>
2024-08-16 17:08:45 +0900

    JSX 서용한 코드와 사용하지 않은 코드의 차이점

--- pracrice/src/01/Book.jsx
+++ pracrice/src/01/Book.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* JSX를 사용한 코드 */
+/* import React from "react";
 
 function Book(props) {
     return(        <div>
@@ -6,6 +7,31 @@ function Book(props) {
             <h2>{`이 책은 총 ${props.num0fpage}페이지로 이루어져 있습니다.`}</h2>
         </div>
     );
+}
+
+export default Book; */
+
+
+/* JSX를 사용하지 않은 코드 */
+import React from "react";
+
+function Book(props){
+    return React.createElement(
+        'div',
+        null,
+        [
+            React.createElement(
+                'div',
+                null,
+                `이 책의 이름은 ${props.name}입니다.`
+        ),
+            React.createElement(
+                'h2',
+                null,
+                `이 책은 총 ${props.num0fPage}페이지로 이뤄져 있습니다.`
+            )
+        ]
+    )
 }
 
 export default Book;
