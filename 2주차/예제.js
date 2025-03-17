1143dee71900118d5140ba5bc6f25ffe8d25e8dc
dpqlsn <a01037111209@gmail.com>
2025-03-17 10:48:59 +0900

    2

--- 2주차/예제.js
+++ 2주차/예제.js
@@ -107,3 +107,61 @@ function Item({ name, importance }) {
         </div>
         );
     }
+
+// 필터 연습
+
+const chemists = people.filter(person =>
+    person.profession === 'chemist'
+);
+
+// 리스트를 둘로 나누기
+
+    import { people } from './data.js';
+    import { getImageUrl } from './utils.js';
+
+    export default function List() {
+    const chemists = people.filter(person =>
+        person.profession === 'chemist'
+    );
+    const everyoneElse = people.filter(person =>
+        person.profession !== 'chemist'
+    );
+    return (
+        <article>
+        <h1>Scientists</h1>
+        <h2>Chemists</h2>
+        <ul>
+            {chemists.map(person =>
+            <li key={person.id}>
+                <img
+                src={getImageUrl(person)}
+                alt={person.name}
+                />
+                <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+                // 데이터를 리스트 아이템의 배열에 넣어줌
+                </p>
+            </li>
+            )}
+        </ul>
+        <h2>Everyone Else</h2>
+        <ul>
+            {everyoneElse.map(person =>
+            <li key={person.id}>
+                <img
+                src={getImageUrl(person)}
+                alt={person.name}
+                />
+                <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+                </p>
+            </li>
+            )}
+        </ul>
+        </article>
+    );
+    }
