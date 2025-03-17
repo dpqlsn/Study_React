0e8d54fccb9ad2d2b321f2f46ad48763e2153637
dpqlsn <a01037111209@gmail.com>
2025-03-17 11:23:06 +0900

    3

--- 2주차/예제.js
+++ 2주차/예제.js
@@ -165,3 +165,52 @@ const chemists = people.filter(person =>
         </article>
     );
     }
+
+// 하나의 컴포넌트에 중첩된 리스트
+import { recipes } from './data.js';
+
+export default function RecipeList() {
+    return (
+        <div>
+        <h1>Recipes</h1>
+        {recipes.map(recipe =>
+            <div key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <ul>
+                {recipe.ingredients.map(
+            img => <li>{img}</li>
+                )}
+            </ul>
+            </div>
+        )}
+        </div>
+    );
+    }
+
+// 리스트 항목 컴포넌트 추출하기
+
+import { recipes } from './data.js';
+
+function Recipe({ id, name, ingredients }) {
+    return (
+        <div>
+        <h2>{name}</h2>
+        <ul>
+            {ingredients.map(ingredient =>
+            <li key={ingredient}>
+                {ingredient}
+            </li>
+            )}
+        </ul>
+        </div>
+    );}
+
+    export default function RecipeList() {
+    return (
+        <div>
+        <h1>Recipes</h1>
+        {recipes.map(recipe =>
+            <Recipe {...recipe} />
+        )}
+        </div>
+    );}
