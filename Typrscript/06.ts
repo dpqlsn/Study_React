21ea098dac6cedfcc5115c9a8fdbd0a4e357dccc
dpqlsn <a01037111209@gmail.com>
2025-03-23 22:43:09 +0900

    06 all

--- Typrscript/06.ts
+++ Typrscript/06.ts
@@ -14,3 +14,25 @@ const heroInfo: HeroProfiles = {
 }
 
 // 조건부 타입
+// 조건부 타입은 조건에 따라 다른 타입을 선택할 수 있는 문법, 마치 자바스크립트의 삼항 연산자처럼 조건을 확인한 후 조건에 따라 다른 타입을 추론
+
+// 조건식 ? 첫번째타입 : 두번째타입
+
+interface Person {
+    name: string;
+    age: number;
+}
+interface Developer extends Person {
+    skill: string;
+}
+
+  type Result = Developer extends Person ? string : number; // string
+
+// infer 
+// 타입스크립트 infer는 조건부 타입에서 미리 정의되지 않은 타입을 유연하게 정의할 수 있게 도와주는 문법
+// 항상 조건부 타입 문법과 같이 사용되며 복잡한 타입 코드를 줄여줌
+
+type ElementType<T> = T extends (infer ArrayElement)[] ? ArrayElement : T
+
+// infer 유효범위
+// 앞에서 infer 뒤에 나오는 타입은 조건부 문법 안에서만 사용
