be76c06a19cee4a32d7981c477b4f8273e4784da
dpqlsn <a01037111209@gmail.com>
2025-03-24 09:57:30 +0900

    예제

---
+++ 3주차/에제.js
@@ -0,0 +1,75 @@
+function AlertButton({ message, children }) {
+    return (
+    <button onClick={() => alert(message)}>
+        {children}
+    </button>
+    );
+}
+
+export default function Toolbar() {
+    return (
+    <div>
+        <AlertButton message="Playing!">
+            Play Movie
+        </AlertButton>
+        <AlertButton message="Uploading!">
+            Upload Image
+        </AlertButton>
+    </div>
+    );
+}
+
+// 이벤트 핸들러 Props로 전달하기
+
+function Button({ onClick, children }) {
+    return (
+        <button onClick={onClick}>
+            {children}
+        </button>
+        );
+    }
+    
+    function PlayButton({ movieName }) {
+        function handlePlayClick() {
+        alert(`Playing ${movieName}!`);
+        }
+    
+        return (
+        <Button onClick={handlePlayClick}>
+            Play "{movieName}"
+        </Button>
+        );
+    }
+    
+    function UploadButton() {
+        return (
+        <Button onClick={() => alert('Uploading!')}>
+            Upload Image
+        </Button>
+        );
+    }
+    
+    export default function Toolbar() {
+        return (
+        <div>
+            <PlayButton movieName="Kiki's Delivery Service" />
+            <UploadButton />
+        </div>
+        );
+    }
+
+// 이벤트 전파
+
+export default function Toolbar() {
+    return (
+    <div className="Toolbar" onClick={() => {
+        alert('You clicked on the toolbar!');
+    }}>
+        <button onClick={() => alert('Playing!')}>
+            Play Movie
+        </button>
+        <button onClick={() => alert('Uploading!')}>
+            Upload Image
+        </button>
+    </div>
+);}
