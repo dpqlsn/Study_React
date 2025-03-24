b50acf5b5edf046b5d550b649a1bf4a16db30f7e
dpqlsn <a01037111209@gmail.com>
2025-03-24 10:01:07 +0900

    이벤트

--- 3주차/에제.js
+++ 3주차/에제.js
@@ -73,3 +73,21 @@ export default function Toolbar() {
         </button>
     </div>
 );}
+
+// 화이트 & 다크모드
+
+export default function LightSwitch() {
+    function handleClick() {
+        let bodyStyle = document.body.style;
+        if (bodyStyle.backgroundColor === 'black') {
+        bodyStyle.backgroundColor = 'white';
+        } else {
+        bodyStyle.backgroundColor = 'black';
+        }
+    }
+
+    return (
+    <button onClick={handleClick}>
+        Toggle the lights
+    </button>
+    );}
